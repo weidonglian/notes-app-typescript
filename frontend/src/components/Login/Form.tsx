9faf1e9a7a459f6342e25a61e0cf78bce5c87307
Weidong Lian <weidong.lian@gmail.com>
2019-12-02 23:53:37 +0100

    login form

--- frontend/src/components/Login/Form.tsx
+++ frontend/src/components/Login/Form.tsx
@@ -1,51 +1,24 @@
-import React from 'react';
-import Avatar from '@material-ui/core/Avatar';
-import Button from '@material-ui/core/Button';
-import CssBaseline from '@material-ui/core/CssBaseline';
-import TextField from '@material-ui/core/TextField';
-import FormControlLabel from '@material-ui/core/FormControlLabel';
-import Checkbox from '@material-ui/core/Checkbox';
-import Link from '@material-ui/core/Link';
-import Paper from '@material-ui/core/Paper';
-import Box from '@material-ui/core/Box';
-import Grid from '@material-ui/core/Grid';
-import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
-import Typography from '@material-ui/core/Typography';
-import { makeStyles } from '@material-ui/core/styles';
+import React from 'react'
+import { Button, Grid, Link, makeStyles } from '@material-ui/core'
+import { TextField, CheckboxWithLabel } from 'formik-material-ui'
+import { Form, Field, FormikProps, withFormik } from 'formik'
+import * as yup from 'yup'
 
-const Copyright: React.FC = () => (
-    <Typography variant="body2" color="textSecondary" align="center">
-      {'Copyright © '}
-      <Link color="inherit" href="https://biosave.org">
-        Notes App
-      </Link>{' '}
-      {new Date().getFullYear()}
-      {'.'}
-    </Typography>
-)
+const formValuesSchema = yup.object({
+  username: yup
+    .string()
+    .required(),
+  password: yup
+    .string()
+    .required(),
+  remember: yup
+    .boolean()
+    .notRequired(),
+})
+
+type FormValues = yup.InferType<typeof formValuesSchema>
 
 const useStyles = makeStyles(theme => ({
-  root: {
-    height: '100vh',
-  },
-  image: {
-    backgroundImage: 'url(https://source.unsplash.com/random)',
-    backgroundRepeat: 'no-repeat',
-    backgroundColor:
-      theme.palette.type === 'dark' ? theme.palette.grey[900] : theme.palette.grey[50],
-    backgroundSize: 'cover',
-    backgroundPosition: 'center',
-  },
-  paper: {
-    margin: theme.spacing(8, 4),
-    display: 'flex',
-    flexDirection: 'column',
-    alignItems: 'center',
-  },
-  avatar: {
-    margin: theme.spacing(1),
-    backgroundColor: theme.palette.secondary.main,
-  },
   form: {
     width: '100%', // Fix IE 11 issue.
     marginTop: theme.spacing(1),
@@ -55,75 +28,81 @@ const useStyles = makeStyles(theme => ({
   },
 }))
 
-export const LoginForm: React.FC = () => {
-  const classes = useStyles();
+interface FormViewProps extends FormikProps<FormValues> {
+
+}
+
+const FormView = (props: FormViewProps) => {
+  const classes = useStyles()
+  const { submitForm } = props
 
   return (
-    <Grid container component="main" className={classes.root}>
-      <CssBaseline />
-      <Grid item xs={false} sm={4} md={7} className={classes.image} />
-      <Grid item xs={12} sm={8} md={5} component={Paper} elevation={6} square>
-        <div className={classes.paper}>
-          <Avatar className={classes.avatar}>
-            <LockOutlinedIcon />
-          </Avatar>
-          <Typography component="h1" variant="h5">
-            Sign in
-          </Typography>
-          <form className={classes.form} noValidate>
-            <TextField
-              variant="outlined"
-              margin="normal"
-              required
-              fullWidth
-              id="email"
-              label="Email Address"
-              name="email"
-              autoComplete="email"
-              autoFocus
-            />
-            <TextField
-              variant="outlined"
-              margin="normal"
-              required
-              fullWidth
-              name="password"
-              label="Password"
-              type="password"
-              id="password"
-              autoComplete="current-password"
-            />
-            <FormControlLabel
-              control={<Checkbox value="remember" color="primary" />}
-              label="Remember me"
-            />
-            <Button
-              type="submit"
-              fullWidth
-              variant="contained"
-              color="primary"
-              className={classes.submit}
-            >
-              Sign In
-            </Button>
-            <Grid container>
-              <Grid item xs>
-                <Link href="#" variant="body2">
-                  Forgot password?
-                </Link>
-              </Grid>
-              <Grid item>
-                <Link href="#" variant="body2">
-                  {"Don't have an account? Sign Up"}
-                </Link>
-              </Grid>
-            </Grid>
-            <Box mt={5}>
-              <Copyright />
-            </Box>
-          </form>
-        </div>
+    <Form className={classes.form}>
+      <Field
+        type='text'
+        label='User name'
+        name="username"
+        component={TextField}
+        autoComplete="username"
+        autoFocus
+        variant="outlined"
+        margin="normal"
+        fullWidth
+      />
+      <Field
+        type="password"
+        label="Password"
+        name="password"
+        component={TextField}
+        variant="outlined"
+        margin="normal"
+        fullWidth
+        autoComplete="current-password"
+      />
+      <Field
+        name='remember'
+        Label={{label: "Remember me"}}
+        component={CheckboxWithLabel}
+        color="primary"
+      />
+      <Button
+        type="submit"
+        fullWidth
+        variant="contained"
+        color="primary"
+        className={classes.submit}
+        onClick={submitForm}
+      >
+        Sign In
+      </Button>
+      <Grid container>
+        <Grid item xs>
+          <Link href="#" variant="body2">
+            Forgot password?
+        </Link>
+        </Grid>
+        <Grid item>
+          <Link href="#" variant="body2">
+            {"Don't have an account? Sign Up"}
+          </Link>
+        </Grid>
       </Grid>
-    </Grid>
+    </Form>
   )
 }
+
+interface LoginFormProps {
+
+}
+
+export const LoginForm = withFormik<LoginFormProps, FormValues>({
+  mapPropsToValues: props => ({
+    username: '',
+    password: '',
+    remember: true
+  }),
+  handleSubmit: values => {
+    console.log(values)
+  },
+  validationSchema: formValuesSchema
+})(FormView)
--- frontend/src/pages/Login.tsx
+++ frontend/src/pages/Login.tsx
@@ -1,7 +1,57 @@
-import React from 'react'
-import { LoginForm } from '../components/Login/Form'
-export const LoginPage = () => (
-    <React.Fragment>
-        <LoginForm />
-    </React.Fragment>
+import React from 'react';
+import Avatar from '@material-ui/core/Avatar';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import Link from '@material-ui/core/Link';
+import Box from '@material-ui/core/Box';
+import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
+import Typography from '@material-ui/core/Typography';
+import { makeStyles } from '@material-ui/core/styles';
+import Container from '@material-ui/core/Container';
+import { LoginForm } from '../components/Login/Form';
+
+const Copyright: React.FC = () => (
+  <Typography variant="body2" color="textSecondary" align="center">
+    {'Copyright © '}
+      <Link color="inherit" href="https://biosave.org">
+        Biosave
+      </Link>{' '}
+    {new Date().getFullYear()}
+    {'.'}
+  </Typography>
 )
+
+const useStyles = makeStyles(theme => ({
+  paper: {
+    marginTop: theme.spacing(8),
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+  },
+  avatar: {
+    margin: theme.spacing(1),
+    backgroundColor: theme.palette.secondary.main,
+  }
+}))
+
+export const LoginPage: React.FC = () => {
+
+  const classes = useStyles();
+
+  return (
+    <Container component="main" maxWidth="xs">
+      <CssBaseline />
+      <div className={classes.paper}>
+        <Avatar className={classes.avatar}>
+          <LockOutlinedIcon />
+        </Avatar>
+        <Typography component="h1" variant="h5">
+          Sign in
+        </Typography>
+        <LoginForm />
+      </div>
+      <Box mt={8}>
+        <Copyright />
+      </Box>
+    </Container>
+  )
+}
