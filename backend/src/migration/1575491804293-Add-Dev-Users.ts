0b8de288f2d53cad23f4735ee4d8f726ffab9df1
Weidong Lian <weidong.lian@gmail.com>
2019-12-04 21:47:41 +0100

    update migrations for dev database

--- backend/src/migration/1575491804293-Add-Dev-Users.ts
+++ backend/src/migration/1575491804293-Add-Dev-Users.ts
@@ -1,7 +1,8 @@
-import { getRepository, MigrationInterface, QueryRunner } from 'typeorm'
-import { User } from '../entity/User'
+import {MigrationInterface, QueryRunner, getRepository} from "typeorm";
+import { User } from '../entity/User';
 
-export class CreateAdminUser1547919837483 implements MigrationInterface {
+export class AddDevUsers1575491804293 implements MigrationInterface {
+
     public async up(queryRunner: QueryRunner): Promise<any> {
         const users = [
             {
@@ -35,4 +36,5 @@ export class CreateAdminUser1547919837483 implements MigrationInterface {
 
     public async down(queryRunner: QueryRunner): Promise<any> {
     }
+
 }
