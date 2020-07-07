547760959931a44565771c85a0a852db8e3deb5e
Weidong Lian <weidong.lian@gmail.com>
2020-07-07 23:16:50 +0200

    minor formatting fixup

--- backend/src/migration/sqls/20200707203204-add-table-users-up.sql
+++ backend/src/migration/sqls/20200707203204-add-table-users-up.sql
@@ -2,6 +2,6 @@
 CREATE TABLE users (
     user_id serial PRIMARY KEY,
     username VARCHAR (50) UNIQUE NOT NULL,
-	  password VARCHAR (50) NOT NULL,
+	password VARCHAR (50) NOT NULL,
     role VARCHAR (50)
 );
