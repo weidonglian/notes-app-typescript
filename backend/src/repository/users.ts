76efedf531fb8c8642257eeb30cf5470f6c98659
Weidong Lian <weidong.lian@gmail.com>
2020-07-07 23:17:02 +0200

    add users repository

--- backend/src/repository/users.ts
+++ backend/src/repository/users.ts
@@ -30,47 +30,6 @@ export class UsersRepository {
         */
     }
 
-    // Creates the table;
-    async create(): Promise<null> {
-        return this.db.none(`
-            CREATE TABLE user(
-                id serial PRIMARY KEY,
-                name text NOT NULL
-            )
-        `)
-    }
-
-    // Initializes the table with some user records, and return their id-s;
-    async init(): Promise<number[]> {
-        // #todo
-        const users = [
-            {
-                username: 'admin',
-                password: 'admin',
-                role: "ADMIN",
-            },
-            {
-                username: 'dev',
-                password: 'dev',
-                role: "USER"
-            },
-            {
-                username: 'test',
-                password: 'test',
-                role: "USER"
-            }
-        ]
-        return this.db.map(`
-            INSERT INTO user(name) VALUES
-            ('Demo User 1'), -- user 1;
-            ('Demo User 2'), -- user 2;
-            ('Demo User 3'), -- user 3;
-            ('Demo User 4'), -- user 4;
-            ('Demo User 5') -- user 5;
-            RETURNING id
-        `, [], (row: { id: number }) => row.id);
-    }
-
     hashPassword(user: User) {
         user.password = bcrypt.hashSync(user.password, 8)
     }
@@ -79,47 +38,42 @@ export class UsersRepository {
         return bcrypt.compareSync(unencryptedPassword, user.password)
     }
 
-    // Drops the table;
-    async drop(): Promise<null> {
-        return this.db.none('DROP TABLE user');
-    }
-
     // Removes all records from the table;
     async clear(): Promise<null> {
-        return this.db.none('TRUNCATE TABLE user CASCADE');
+        return this.db.none('TRUNCATE TABLE users CASCADE');
     }
 
     // Adds a new user, and returns the new object;
-    async add(name: string): Promise<User> {
+    async add(username: string, password: string, role: string): Promise<User> {
         return this.db.one(`
-            INSERT INTO user(name)
-            VALUES($1)
+            INSERT INTO users (username, password, role)
+            VALUES($1, $2, $3)
             RETURNING *
-        `, name);
+        `, [username, password, role]);
     }
 
     // Tries to delete a user by id, and returns the number of records deleted;
     async remove(id: number): Promise<number> {
-        return this.db.result('DELETE FROM user WHERE id = $1', +id, (r: IResult) => r.rowCount);
+        return this.db.result('DELETE FROM users WHERE id = $1', +id, (r: IResult) => r.rowCount);
     }
 
     // Tries to find a user from id;
     async findById(id: number): Promise<User | null> {
-        return this.db.oneOrNone('SELECT * FROM user WHERE id = $1', +id);
+        return this.db.oneOrNone('SELECT * FROM users WHERE id = $1', +id);
     }
 
     // Tries to find a user from name;
     async findByName(name: string): Promise<User | null> {
-        return this.db.oneOrNone('SELECT * FROM user WHERE name = $1', name);
+        return this.db.oneOrNone('SELECT * FROM users WHERE name = $1', name);
     }
 
     // Returns all user records;
     async all(): Promise<User[]> {
-        return this.db.any('SELECT * FROM user');
+        return this.db.any('SELECT * FROM users');
     }
 
     // Returns the total number of users;
     async total(): Promise<number> {
-        return this.db.one('SELECT count(*) FROM user', [], (a: { count: string }) => +a.count);
+        return this.db.one('SELECT count(*) FROM users', [], (a: { count: string }) => +a.count);
     }
 }
